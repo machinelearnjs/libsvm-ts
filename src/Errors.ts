3c086fa36e1d50033157f58e322a18c4b626dbb0
Jason Shin <visualbbasic@gmail.com>
2019-04-27 22:02:54 +1000

    refactor(util.js): refactoring to ts

---
+++ src/Errors.ts
@@ -0,0 +1,9 @@
+/**
+ * The error is used when a matrix does not contain a consistent type for its elements
+ * @ignore
+ */
+export const UtilError = function(message) {
+  Error.captureStackTrace(this, this.constructor);
+  this.name = this.constructor.name;
+  this.message = message;
+};
