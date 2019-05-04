35ef56cc7df04a9c088d0584330a930773ba4318
Jason Shin <visualbbasic@gmail.com>
2019-05-05 00:30:13 +1000

    refactor(example/bodyfat.js): refactoring according to the new API

--- examples/bodyfat.js
+++ examples/bodyfat.js
@@ -2,40 +2,27 @@
 
 const fs = require('fs');
 const path = require('path');
-
 const SVM = require('..');
 
-SVM.then((S) => {
-  const svm = new S({
-    type: 'C_SVC',
-    kernel: 'LINEAR',
-    epsilon: 0.001,
-    quiet: false,
-    probabilityEstimates: true
-  });
-  svm.train(['test'], [1]);
-});
-
-/*let data = fs.readFileSync(
+let data = fs.readFileSync(
   path.join(__dirname, './bodyfat_scale.txt'),
   'utf-8'
 );
+
 data = data.split('\n').map((line) => line.split(' ').filter((el) => el));
 let labels = data.map((line) => +line.splice(0, 1)[0]);
 const features = data.map((line) => line.map((el) => +el.split(':')[1]));
 
 const svm = new SVM({
-  type: SVM.SVM_TYPES.EPSILON_SVR,
-  kernel: SVM.KERNEL_TYPES.RBF,
+  type: 'C_SVC',
+  kernel: 'LINEAR',
   epsilon: 0.001,
   quiet: false,
   probabilityEstimates: true
 });
 
-svm.train(features, labels);
-console.log(svm.predictInterval(features, 0.99));
-fs.writeFileSync(path.join(__dirname, 'bodyfat.model'), svm.serializeModel());
-
-// svm.crossValidation(features, labels);
-*/
-
+svm.load().then((loadedSVM) => {
+  loadedSVM.train({ samples: features, labels});
+  console.log(svm.predictInterval({ samples: features, confidence: 0.99 }));
+  fs.writeFileSync(path.join(__dirname, 'bodyfat.model'), svm.serializeModel());
+});
