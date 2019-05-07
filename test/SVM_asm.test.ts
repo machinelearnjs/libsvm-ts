8e8af2fd4f3f04c829af3a36a9f2840dbf25eac3
Jason Shin <visualbbasic@gmail.com>
2019-05-07 23:53:32 +1000

    refactor(SVM.test): correcting the tests

--- test/SVM_asm.test.ts
+++ test/SVM_asm.test.ts
@@ -2,34 +2,39 @@ import { SVM } from '../src/SVM';
 import { KernelTypes, SVMTypes } from '../src/types/Commands';
 
 describe('SVM:asm', () => {
-  describe('.train(...)', () => {
-    test('should train using XOR data', () => {
-      const svm = new SVM({
-        type: SVMTypes.C_SVC,
-        kernel: KernelTypes.LINEAR,
-        epsilon: 0.001,
+  test('should train using XOR data using RBF kernel and C_SVC', () => {
+    const svm = new SVM({
+      type: SVMTypes.C_SVC,
+      kernel: KernelTypes.RBF,
+      gamma: 1,
+      cost: 1,
+      quiet: true,
+      probabilityEstimates: true,
+    });
+    const features = [[0, 0], [1, 1], [1, 0], [0, 1]];
+    const labels = [0, 0, 1, 1];
+
+    const expected = {
+      model: 5251664,
+      options: {
+        type: 'C_SVC',
+        kernel: 'RBF',
+        gamma: 1,
+        cost: 1,
         quiet: true,
         probabilityEstimates: true,
-      });
-      const features = [[0, 0], [1, 1], [1, 0], [0, 1]];
-      const labels = [0, 0, 1, 1];
+      },
+      loaded: true,
+    };
 
-      const expected = {
-        model: 5251664,
-        options: {
-          type: 'C_SVC',
-          kernel: 'LINEAR',
-          epsilon: 0.001,
-          quiet: true,
-          probabilityEstimates: true,
-        },
-        loaded: true,
-      };
+    svm.loadASM().then((loadedSVM) => {
+      loadedSVM.train({ samples: features, labels });
+      expect(svm.toJSON()).toEqual(expected);
 
-      svm.loadASM().then((loadedSVM) => {
-        loadedSVM.train({ samples: features, labels });
-        expect(svm.toJSON()).toEqual(expected);
-      });
+      for (let i = 0; i < features.length; i++) {
+        const pred = loadedSVM.predictOne({ sample: features[i] });
+        expect(pred).toBe(labels[i]);
+      }
     });
   });
 });
